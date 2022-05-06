bdb7626b46781630515c31f2658945cf16e60721
hhngg <101886098+hhngg@users.noreply.github.com>
2022-05-06 11:57:16 +0800

    Create main.py

---
+++ 0002 join string/main.py
@@ -0,0 +1,18 @@
+def join(str1, str2, p):
+    if p == 0:
+        return print(str1+str2)
+    elif p == 1:
+        return print(str2+str1)
+    else:
+        return print("wrong number, only type 0 or 1")
+
+join("ABC", "CDE", 0)
+join("ABC", "CDE", 1)
+join("ABC", "CDE", 2)
+
+"""
+result:
+ABCDEF
+DEFABC
+wrong number, only type 0 or 1
+"""
