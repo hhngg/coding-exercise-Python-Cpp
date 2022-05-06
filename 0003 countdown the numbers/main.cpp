025d4ad1be11439cc66ea8406822ba2615ed5366
hhngg <101886098+hhngg@users.noreply.github.com>
2022-05-06 11:58:21 +0800

    Create main.cpp

---
+++ 0003 countdown the numbers/main.cpp
@@ -0,0 +1,33 @@
+//Write a program that prints out the
+//calls for a spaceship that is about to
+//launch. Countdown the numbers from
+//10 to 1 and then write “Liftoff.”
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  for (int i = 10; i > 0; i--) {
+    cout << i << "\n";
+  }
+  cout << "Liftoff" << endl;
+}
+
+#####################################
+#10                                 #
+#9                                  #
+#8                                  #
+#7                                  #
+#6                                  #
+#5                                  #
+#4                                  #
+#3                                  #
+#2                                  #
+#Liftoff                            #
+#####################################
+
+    © 2022 GitHub, Inc.
+
+    Terms
+    Privacy
+    Security
