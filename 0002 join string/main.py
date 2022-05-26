4029ed3fd7967fb5e9ac6b5589ae1ecb06688070
hhngg <101886098+hhngg@users.noreply.github.com>
2022-05-26 15:29:56 +0800

    Update main.py

--- 0002 join string/main.py
+++ 0002 join string/main.py
@@ -1,10 +1,13 @@
-def join(str1, str2, p):
-    if p == 0:
-        return print(str1+str2)
-    elif p == 1:
-        return print(str2+str1)
-    else:
-        return print("wrong number, only type 0 or 1")
+class solution(object):
+    def join(self, str1, str2, p):
+        if p == 0:
+            return print(str1+str2)
+        elif p == 1:
+            return print(str2+str1)
+        else:
+            return print("wrong number, only type 0 or 1")
+
+solution().join("ABC", "CDE", 1)
 
 join("ABC", "CDE", 0)
 join("ABC", "CDE", 1)
